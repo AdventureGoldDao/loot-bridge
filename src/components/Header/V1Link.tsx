45018459acb1f38fa440d71c13da6e362acb7e4b
billy <>
2023-11-30 14:43:37 +0800

    update: v1 v2 link

--- src/components/Header/V1Link.tsx
+++ src/components/Header/V1Link.tsx
@@ -1,6 +1,16 @@
-import { Stack, Typography } from '@mui/material'
+import { Menu, MenuItem, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
+import { ReactComponent as Icon } from '../../pages/components/arrow_icon.svg'
 
 export default function V1Link() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
     <Stack
       justifyContent={'center'}
@@ -20,13 +30,52 @@ export default function V1Link() {
         borderRadius: '20px',
         '& svg path': {
           fill: '#A5FFBE'
+        },
+        '& .MuiMenu-root#basic-menu': {
+          width: '70px !important'
         }
       }}
-      onClick={() => window.open('https://mainnet.lootchain.com/bridge', '_blank')}
     >
-      <Typography fontSize={14} fontWeight={700} color={'#A5FFBE'}>
+      <Typography
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        fontSize={14}
+        fontWeight={700}
+        color={'#A5FFBE'}
+      >
         V1
+        <Icon style={{ marginLeft: '5px' }} />
       </Typography>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        sx={{
+          '& .MuiList-root': {
+            borderRadius: '10px',
+            border: '1px solid #A5FFBE',
+            background: '#020202'
+          },
+          '& .MuiMenuItem-root:hover': {
+            background: '#4B5954'
+          },
+          '& .MuiMenuItem-root': {
+            color: '#A5FFBE',
+            background: '#020202',
+            textAlign: 'center',
+            minWidth: '50px !important'
+          }
+        }}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        <MenuItem onClick={() => window.open('https://mainnet.lootchain.com/bridge', '_blank')}>V1</MenuItem>
+        <MenuItem onClick={handleClose}>V2</MenuItem>
+      </Menu>
     </Stack>
   )
 }
